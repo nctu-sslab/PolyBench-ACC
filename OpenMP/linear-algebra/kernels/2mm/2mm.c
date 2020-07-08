4e92b666888a3e321b4c6bbe6e03ba1013ea1480
ponsheng <a7061120@gmail.com>
2020-07-08 12:19:29 +0800

    [openmp] Add offloading to 2mm

--- OpenMP/linear-algebra/kernels/2mm/2mm.c
+++ OpenMP/linear-algebra/kernels/2mm/2mm.c
@@ -4,7 +4,7 @@
  *
  * Contact:
  * William Killian <killian@udel.edu>
- * 
+ *
  * Copyright 2013, The University of Delaware
  */
 #include <stdio.h>
@@ -68,6 +68,7 @@ void print_array(int ni, int nl,
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
+#ifndef OMP_OFFLOAD
 static
 void kernel_2mm(int ni, int nj, int nk, int nl,
 		DATA_TYPE alpha,
@@ -102,6 +103,38 @@ void kernel_2mm(int ni, int nj, int nk, int nl,
   }
   #pragma endscop
 }
+#else
+static void kernel_2mm(int ni, int nj, int nk, int nl,
+		DATA_TYPE alpha,
+		DATA_TYPE beta,
+		DATA_TYPE POLYBENCH_2D(tmp,NI,NJ,ni,nj),
+		DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj),
+		DATA_TYPE POLYBENCH_2D(C,NL,NJ,nl,nj),
+		DATA_TYPE POLYBENCH_2D(D,NI,NL,ni,nl))
+{
+  int i, j, k;
+  #pragma omp target data map(to:tmp[:NI][:NJ], A[:NI][:NK], B[:NK][:NJ], C[:NL][:NJ]) \
+    map(tofrom: D[:NI][:NL])
+  {
+    #pragma omp target teams distribute parallel for private (j, k)
+    for (i = 0; i < _PB_NI; i++)
+      for (j = 0; j < _PB_NJ; j++) {
+        tmp[i][j] = 0;
+          for (k = 0; k < _PB_NK; ++k)
+            tmp[i][j] += alpha * A[i][k] * B[k][j];
+      }
+
+    #pragma omp target teams distribute parallel for private (j, k)
+    for (i = 0; i < _PB_NI; i++)
+      for (j = 0; j < _PB_NL; j++) {
+	    D[i][j] *= beta;
+	    for (k = 0; k < _PB_NJ; ++k)
+	      D[i][j] += tmp[i][k] * C[k][j];
+	}
+  }
+}
+#endif
 
 
 int main(int argc, char** argv)
@@ -130,6 +163,7 @@ int main(int argc, char** argv)
 
   /* Start timer. */
   polybench_start_instruments;
+
 
   /* Run kernel. */
   kernel_2mm (ni, nj, nk, nl,
