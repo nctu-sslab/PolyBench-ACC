2254cf6a32a99d2f714ffe999963b5f36f36767b
pschen <a7061120@gmail.com>
2020-07-13 17:16:33 +0800

    [openmp] Add result dir and build output script

---
+++ OpenMP/result/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+result="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $result
+cd ../
+root=`pwd`
+makefiles=`find . -name Makefile`
+
+export RUN_MINI=1
+export VERIFY=1
+
+for m in $makefiles
+do
+    cd $root
+    dir=`dirname $m`
+    echo "Generating output of $dir"
+    src=$root/$dir
+    dest=$result/$dir
+    mkdir -p $src
+
+    stdout=`make -C $src 2>&1`
+    ret=$?
+    if [ $ret -ne 0 ]; then
+            echo -e "* Compiler Error!"
+    fi
+    $src/run 2> $dest/output.txt
+    make clean -C $src &>/dev/null
+done
