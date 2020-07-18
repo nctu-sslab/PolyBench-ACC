9e0bd48790f2f5267ffc25f6c4fdf401c5d152ee
pschen <a7061120@gmail.com>
2020-07-18 14:37:01 +0800

    [openmp][result] Add ignore, revise script

--- OpenMP/result/build.sh
+++ OpenMP/result/build.sh
@@ -8,21 +8,40 @@ makefiles=`find . -name Makefile`
 
 export RUN_MINI=1
 export RUN_DUMP=1
+export RUN_NOOMP=1
 
 for m in $makefiles
 do
     cd $root
     dir=`dirname $m`
-    echo "Generating output of $dir"
+
+    if [[ $dir == "./result" ]]; then
+        continue
+    fi
+
     src=$root/$dir
     dest=$result/$dir
-    mkdir -p $src
+
+    if [[ -s $dest/output.txt ]]; then
+        echo "$dir/output.txt exists"
+        continue
+    fi
+
+    echo "Generating $dir/output.txt"
+    mkdir -p $dest
 
     stdout=`make -C $src 2>&1`
     ret=$?
     if [ $ret -ne 0 ]; then
-            echo -e "* Compiler Error!"
+        echo -e "* Compiler Error!"
+        continue
+        make clean -C $src &>/dev/null
     fi
     $src/run 2> $dest/output.txt
+    ret=$?
+    if [[ ! $ret -eq 0 ]]; then
+        echo -e "* Runtime Error!"
+        rm -f $dest/output.txt
+    fi
     make clean -C $src &>/dev/null
 done
